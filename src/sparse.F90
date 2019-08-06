66660f67e2d9dca910f0664df91c34e3a5e2a30f
macwozni <macwozni>
2019-08-06 07:55:09 +0200

    fixed with pointers, still without interface to MUMPS

--- src/sparse.F90
+++ src/sparse.F90
@@ -22,29 +22,34 @@ type sparse_matrix
     type (sparse_matrix_line), pointer :: first
 end type sparse_matrix
 
+
+
 contains
 
-subroutine initialize(x,y,matrix)
+
+
+subroutine initialize_sparse(x,y,matrix)
     implicit none
     integer (kind=4), intent(in) :: x
     integer (kind=4), intent(in) :: y
-    type (sparse_matrix), intent(out) :: matrix
+    type (sparse_matrix), pointer, intent(out) :: matrix
     
+    allocate(matrix)
     matrix%x = x
     matrix%y = y
     matrix%total_entries = 0
     matrix%first => NULL()
-end subroutine initialize
+end subroutine initialize_sparse
 
 
 subroutine find(matrix,x,y,entr)
     implicit none
-    type(sparse_matrix), intent(inout) :: matrix
+    type(sparse_matrix), pointer, intent(inout) :: matrix
     integer (kind=4), intent(in) :: x
     integer (kind=4), intent(in) :: y
-    type(sparse_matrix_entry), intent(out)    :: entr
-    type(sparse_matrix_line)  :: line
-    type(sparse_matrix_entry) :: tmp
+    type(sparse_matrix_entry), pointer, intent(out)    :: entr
+    type(sparse_matrix_line), pointer  :: line
+    type(sparse_matrix_entry), pointer :: tmp
     type(sparse_matrix_entry), pointer :: tmp2
     
     call find_line(matrix,x,line)
@@ -55,15 +60,15 @@ subroutine find(matrix,x,y,entr)
         line%first%y = y
         line%first%next => NULL()
         line%first%val = 0.d0
-        entr = line%first
+        entr => line%first
         return
     end if
     
-    tmp = line%first
+    tmp => line%first
     
     do while (associated(tmp%next))
         if (tmp%next%y .EQ. y) then
-            entr = tmp%next
+            entr => tmp%next
             return
         end if
         if (tmp%next%y .GT. y) then
@@ -73,12 +78,11 @@ subroutine find(matrix,x,y,entr)
             tmp%next%y = y
             tmp%next%next => tmp2
             tmp%val = 0.d0
-            entr = tmp%next
+            entr => tmp%next
             return
         end if
-        tmp = tmp%next
+        tmp => tmp%next
     end do
-    
 end subroutine find
 
 
@@ -86,8 +90,8 @@ subroutine find_line(matrix,x,line)
     implicit none
     type(sparse_matrix), intent(inout) :: matrix
     integer (kind=4), intent(in) :: x
-    type(sparse_matrix_line), intent(out) :: line
-    type(sparse_matrix_line) :: tmp
+    type(sparse_matrix_line), pointer, intent(out) :: line
+    type(sparse_matrix_line), pointer :: tmp
     type(sparse_matrix_line), pointer :: tmp2
     
     if (.NOT.associated(matrix%first)) then
@@ -95,15 +99,15 @@ subroutine find_line(matrix,x,line)
         matrix%first%x = x
         matrix%first%next => NULL()
         matrix%first%first => NULL()
-        line = matrix%first
+        line => matrix%first
         return
     endif
     
-    tmp = matrix%first
+    tmp => matrix%first
     
     do while (associated(tmp%next))
         if (tmp%next%x .EQ. x) then
-            line = tmp%next
+            line => tmp%next
             return
         end if
         if (tmp%next%x .GT. x) then
@@ -112,30 +116,28 @@ subroutine find_line(matrix,x,line)
             tmp%next%x = x
             tmp%next%next => tmp2
             tmp%next%first => NULL()
-            line = tmp%next
+            line => tmp%next
             return
         end if
-        tmp = tmp%next
+        tmp => tmp%next
     end do
     
     allocate(tmp%next)
     tmp%next%x = x
     tmp%next%next => NULL()
     tmp%next%first => NULL()
-    line = tmp%next
-    return
-    
+    line => tmp%next
 end subroutine find_line
 
 
 
 subroutine add(matrix,x,y,val)
     implicit none
-    type(sparse_matrix), intent(inout) :: matrix
+    type(sparse_matrix), pointer, intent(inout) :: matrix
     integer (kind=4), intent(in) :: x
     integer (kind=4), intent(in) :: y
-    real (kind=8), intent(out) :: val
-    type(sparse_matrix_entry) :: entr
+    real (kind=8), intent(in) :: val
+    type(sparse_matrix_entry), pointer :: entr
     
     call find(matrix,x,y,entr)
     entr%val = entr%val + val
@@ -144,12 +146,13 @@ end subroutine add
 
 subroutine clear_matrix(matrix)
     implicit none
-    type(sparse_matrix), intent(inout) :: matrix
+    type(sparse_matrix), pointer, intent(inout) :: matrix
+    type(sparse_matrix_line), pointer :: line
     
-    matrix%x = 0
-    matrix%y = 0
-    matrix%total_entries = 0
-    call clear_line(matrix%first)
+    if(.NOT. associated(matrix)) return
+    line => matrix%first
+    deallocate(matrix)
+    call clear_line(line)
 end subroutine clear_matrix
 
 
@@ -178,6 +181,27 @@ recursive subroutine clear_entry(entr)
     deallocate(entr)
     call clear_entry(tmp)
 end subroutine clear_entry
+
+
+subroutine to_dense_matrix(matrix, dmatrix)
+    implicit none
+    type(sparse_matrix), intent(in) :: matrix
+    real (kind=8), dimension(matrix%x,matrix%y), intent(out) :: dmatrix
+    type(sparse_matrix_line), pointer :: line
+    type(sparse_matrix_entry), pointer :: entr
+    
+    dmatrix = 0.d0
+    line => matrix%first
+    
+    do while (associated(line))
+        entr => line%first
+        do while(associated(entr))
+            dmatrix(entr%x, entr%y) = entr%val
+            entr => entr%next
+        end do
+        line => line%next
+    end do
+end subroutine to_dense_matrix
 
 end module sparse
     
