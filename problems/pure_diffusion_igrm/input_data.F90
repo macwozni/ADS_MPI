78272e885c02f488fcaab2b9bbb660ef478a9167
macwozni <macwozni>
2019-08-15 09:50:36 +0200

    pure diffusion - starting coding

---
+++ problems/pure_diffusion_igrm/input_data.F90
@@ -0,0 +1,94 @@
+module input_data
+
+   implicit none
+
+   ! Curve number and lentgh
+   integer(kind = 4), parameter :: cN = 30, cL = 16
+   real (kind = 8), parameter :: radius = 0.15, pumping_strength = 1, draining_strength = 1
+
+   !!! krzywe dane przez segmenty
+   real (kind = 8) :: cx(cN * cL), cy(cN * cL), cz(cN * cL)
+   real (kind = 8) :: mi = 10.d0
+   real (kind = 8) :: GROUND = 0.2
+   real (kind = 8), parameter :: Kqmin = 1.d0, Kqmax = 1000.d0
+   integer(kind = 4) :: npumps, ndrains
+   real (kind = 8), allocatable, dimension(:,:) :: pumps, drains
+
+   ! Buffer for values of permeability function
+   real (kind = 8), allocatable :: Kqvals(:,:,:,:,:,:)
+
+   ! Current time
+   real (kind = 8) :: t
+
+   ! Time and timestep
+   real (kind = 8) :: Dt
+
+   ! Number of iterations
+   integer :: steps
+
+   ! Statistics computed during the simulation
+   real (kind = 8) :: pollution = 0
+
+   ! order of approximations
+   integer(kind = 4) :: ORDER
+
+   ! number of elements in one dimension
+   integer(kind = 4) :: SIZE
+
+   integer(kind = 4) :: procx, procy, procz
+
+   real (kind = 8) :: drained = 0
+
+
+contains
+
+
+   ! -------------------------------------------------------------------
+   ! Sets values of parameters (order and size)
+   ! -------------------------------------------------------------------
+   subroutine InitializeParameters
+      implicit none
+      character(100) :: input
+      integer(kind = 4) :: length
+      integer(kind = 4) :: status
+
+      ! ./l2 <size> <procx> <procy> <procz> <nsteps> <dt>
+      ORDER = 2
+
+      call GET_COMMAND_ARGUMENT(1, input, length, status)
+      read(input, *) SIZE
+      call GET_COMMAND_ARGUMENT(2, input, length, status)
+      read(input, *) ORDER
+      call GET_COMMAND_ARGUMENT(3, input, length, status)
+      read(input, *) procx
+      call GET_COMMAND_ARGUMENT(4, input, length, status)
+      read(input, *) procy
+      call GET_COMMAND_ARGUMENT(5, input, length, status)
+      read(input, *) procz
+      call GET_COMMAND_ARGUMENT(6, input, length, status)
+      read(input, *) steps
+      call GET_COMMAND_ARGUMENT(7, input, length, status)
+      read(input, *) Dt
+
+   end subroutine InitializeParameters
+
+
+
+
+
+   ! forcing
+   ! x, y, z - point in space
+   function forcing(x, y, z) result (fval)
+      implicit none
+      real (kind = 8) :: x, y, z
+      real (kind = 8) :: fval
+      integer(kind = 4) :: i
+      real (kind = 8), dimension(3) :: p1
+
+
+
+   end function forcing
+
+
+
+end module input_data
---
+++ problems/pure_diffusion_igrm/main.F90
@@ -0,0 +1,83 @@
+
+
+program main
+
+   use Setup, ONLY: ADS_Setup, ADS_compute_data
+   use parallelism, ONLY: MYRANK
+   use parallelism, ONLY: PRINTRANK, InitializeParallelism, CleanParallelism
+   use communicators, ONLY: CreateCommunicators
+   use RHS_eq
+   use ADSS
+   use input_data
+   use mpi
+
+   implicit none
+
+
+   ! Iteration counter
+   integer :: iter = 0
+
+   integer(kind = 4) :: ierr
+   integer(kind = 4), dimension(3) :: p1, p2
+
+   type (ADS_setup) :: ads
+   type (ADS_compute_data) :: ads_data
+
+
+   integer :: values(1:8), k
+   integer, dimension(:), allocatable :: seed
+   real(8) :: r
+   real (kind = 8) :: l2norm, fullnorm
+
+   !call date_and_time(values = values)
+   !values = (/ 0.d8, 0.d7, 0.d6, 0.d5, 0.d4, 0.d3, 0.d2, 0.d1 /)
+   !call random_seed(size = k)
+   !allocate(seed(1:k))
+   !seed(:) = values(3)
+   !call random_seed(put = seed)
+
+
+#ifdef DEBUG
+   write (*, *) 'debug'
+#endif
+
+   t = 0
+
+   ! -------------------------------------------------------------------
+   ! Code
+   ! -------------------------------------------------------------------
+
+   call InitializeParameters
+
+   ! prepare the problem dimensions
+
+   call InitializeParallelism(procx, procy, procz, ierr)
+   call CreateCommunicators(ierr)
+   p1 = (/ SIZE, SIZE, SIZE /)
+   p2 = (/ ORDER, ORDER, ORDER /)
+   call Initialize(p1, p2, ads, ads_data, ierr)
+   allocate(Kqvals(ads % p(1) + 1, ads % p(2) + 1, ads % p(3) + 1, ads % maxe(1) - ads % mine(1) + 1, &
+   ads % maxe(2) - ads % mine(2) + 1, ads % maxe(3) - ads % mine(3) + 1))
+   call InitInputData
+   call PrecomputeKq(ads)
+
+   ! Iterations
+   do iter = 0, steps
+
+      l2norm = 0.d0
+      call Step(iter, ComputePointForRHS, ads, ads_data, l2norm, ierr)
+      call MPI_Reduce(l2norm, fullnorm, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+      if (MYRANK == 0) then
+         write(*, *) iter
+         write(*, *) fullnorm
+         write(*, *) iter, 'L2 norm:', fullnorm
+      endif
+      t = t + Dt
+
+   enddo
+
+   call ComputeResults
+   call Cleanup(ads, ads_data, ierr)
+   call CleanParallelism(ierr)
+
+end program main
