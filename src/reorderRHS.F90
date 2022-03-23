b100f7e98b86a66d54ffff099b1f493879fd9445
macwozni <macwozni@Komeda>
2022-03-22 21:44:14 -0300

    stylistical changes to reorderRHS.F90

--- src/reorderRHS.F90
+++ src/reorderRHS.F90
@@ -6,12 +6,10 @@ contains
 
    subroutine ReorderRHSForY(ibeg, iend, F, F2)
       implicit none
-      integer(kind = 4), dimension(3), intent(in) :: ibeg
-      integer(kind = 4), dimension(3), intent(in) :: iend
-      real (kind = 8), intent(in) :: F(0:(iend(1) - ibeg(1) + 1) - 1, &
-      0:(iend(2) - ibeg(2) + 1)*(iend(3) - ibeg(3) + 1) - 1)
-      real (kind = 8), intent(out) :: F2(0:(iend(2) - ibeg(2) + 1) - 1, &
-      0:(iend(1) - ibeg(1) + 1)*(iend(3) - ibeg(3) + 1) - 1)
+      integer(kind = 4), dimension(:), intent(in) :: ibeg
+      integer(kind = 4), dimension(:), intent(in) :: iend
+      real (kind = 8), dimension(:,:), intent(in) :: F
+      real (kind = 8), dimension(:,:), intent(out) :: F2
       integer(kind = 4) :: ix, iy, iz
       integer(kind = 4) :: ind2, ind13, ind1, ind23
 
@@ -19,11 +17,11 @@ contains
          do iy = ibeg(2), iend(2)
             do iz = ibeg(3), iend(3)
 
-               ind2 = iy - ibeg(2)
-               ind13 = (ix - ibeg(1))+(iz - ibeg(3))*(iend(1) - ibeg(1) + 1)
+               ind2 = iy - ibeg(2)+1
+               ind13 = (ix - ibeg(1))+(iz - ibeg(3))*(iend(1) - ibeg(1) + 1)+1
 
-               ind1 = ix - ibeg(1)
-               ind23 = (iy - ibeg(2))+(iz - ibeg(3))*(iend(2) - ibeg(2) + 1)
+               ind1 = ix - ibeg(1)+1
+               ind23 = (iy - ibeg(2))+(iz - ibeg(3))*(iend(2) - ibeg(2) + 1)+1
 
                F2(ind2, ind13) = F(ind1, ind23)
             enddo
@@ -35,12 +33,10 @@ contains
 
    subroutine ReorderRHSForZ(ibeg, iend, F, F2)
       implicit none
-      integer(kind = 4), dimension(3), intent(in) :: ibeg
-      integer(kind = 4), dimension(3), intent(in) :: iend
-      real (kind = 8), intent(in) :: F(0:(iend(2) - ibeg(2) + 1) - 1, &
-      0:(iend(1) - ibeg(1) + 1)*(iend(3) - ibeg(3) + 1) - 1)
-      real (kind = 8), intent(out) :: F2(0:(iend(3) - ibeg(3) + 1) - 1, &
-      0:(iend(1) - ibeg(1) + 1)*(iend(2) - ibeg(2) + 1) - 1)
+      integer(kind = 4), dimension(:), intent(in) :: ibeg
+      integer(kind = 4), dimension(:), intent(in) :: iend
+      real (kind = 8), dimension(:,:), intent(in) :: F
+      real (kind = 8), dimension(:,:), intent(out) :: F2
       integer(kind = 4) :: ix, iy, iz
       integer(kind = 4) :: ind3, ind12, ind2, ind13
 
@@ -48,11 +44,11 @@ contains
          do iy = ibeg(2), iend(2)
             do iz = ibeg(3), iend(3)
 
-               ind3 = iz - ibeg(3)
-               ind12 = (ix - ibeg(1))+(iy - ibeg(2))*(iend(1) - ibeg(1) + 1)
+               ind3 = iz - ibeg(3)+1
+               ind12 = (ix - ibeg(1))+(iy - ibeg(2))*(iend(1) - ibeg(1) + 1)+1
 
-               ind2 = iy - ibeg(2)
-               ind13 = (ix - ibeg(1))+(iz - ibeg(3))*(iend(1) - ibeg(1) + 1)
+               ind2 = iy - ibeg(2)+1
+               ind13 = (ix - ibeg(1))+(iz - ibeg(3))*(iend(1) - ibeg(1) + 1)+1
 
                F2(ind3, ind12) = F(ind2, ind13)
 
@@ -65,12 +61,10 @@ contains
 
    subroutine ReorderRHSForX(ibeg, iend, F, F2)
       implicit none
-      integer(kind = 4), dimension(3), intent(in) :: ibeg
-      integer(kind = 4), dimension(3), intent(in) :: iend
-      real (kind = 8), intent(in) :: F(0:(iend(3) - ibeg(3) + 1) - 1, &
-      0:(iend(1) - ibeg(1) + 1)*(iend(2) - ibeg(2) + 1) - 1)
-      real (kind = 8), intent(out) :: F2(0:(iend(1) - ibeg(1) + 1) - 1, &
-      0:(iend(2) - ibeg(2) + 1)*(iend(3) - ibeg(3) + 1) - 1)
+      integer(kind = 4), dimension(:), intent(in) :: ibeg
+      integer(kind = 4), dimension(:), intent(in) :: iend
+      real (kind = 8), dimension(:,:), intent(in) :: F
+      real (kind = 8), dimension(:,:), intent(out) :: F2
       integer(kind = 4) :: ix, iy, iz
       integer(kind = 4) :: ind1, ind23, ind3, ind12
 
@@ -78,11 +72,11 @@ contains
          do iy = ibeg(2), iend(2)
             do iz = ibeg(3), iend(3)
 
-               ind1 = ix - ibeg(1)
-               ind23 = (iy - ibeg(2))+(iz - ibeg(3))*(iend(2) - ibeg(2) + 1)
+               ind1 = ix - ibeg(1)+1
+               ind23 = (iy - ibeg(2))+(iz - ibeg(3))*(iend(2) - ibeg(2) + 1)+1
 
-               ind3 = iz - ibeg(3)
-               ind12 = (ix - ibeg(1))+(iy - ibeg(2))*(iend(1) - ibeg(1) + 1)
+               ind3 = iz - ibeg(3)+1
+               ind12 = (ix - ibeg(1))+(iy - ibeg(2))*(iend(1) - ibeg(1) + 1)+1
 
                F2(ind1, ind23) = F(ind3, ind12)
 
