69f3ce99083d9e258a928550425cc5db149928d6
macwozni <macwozni@Komeda>
2022-03-22 22:22:17 -0300

    test problem

--- problems/igrm_L2Projection/main.F90
+++ problems/igrm_L2Projection/main.F90
@@ -6,7 +6,7 @@ program main
    use parallelism, ONLY: MYRANK
    use parallelism, ONLY: PRINTRANK, InitializeParallelism, CleanParallelism
    use communicators, ONLY: CreateCommunicators
-   use RHS_eq
+   use RHS_fun
    use ADSS
    use input_data
    use mpi
@@ -19,17 +19,21 @@ program main
    integer(kind = 4) :: i, j
 
    integer(kind = 4) :: ierr
-   integer(kind = 4), dimension(3) :: p1, p2
+   integer(kind = 4), dimension(3) :: n, p1, p2
    
    logical :: prnt = .FALSE.
    logical :: ok = .TRUE.
 
-   type (ADS_setup) :: ads
+   type (ADS_setup) :: ads_test, ads_trial
    type (ADS_compute_data) :: ads_data
    
    real (kind = 8) :: epsilon = 1.E-10
 
    real (kind = 8) :: l2norm, fullnorm
+
+   real(kind=8) :: mix(4,3)
+   real (kind=8), dimension(7,3) :: alpha_step
+   integer (kind = 4) :: nn
 
 #ifdef DEBUG
    write (*, *) 'debug'
@@ -46,42 +50,49 @@ program main
 
    call InitializeParallelism(procx, procy, procz, ierr)
    call CreateCommunicators(ierr)
-   p1 = (/ isizex, isizey, isizez /)
+   n = (/ isizex, isizey, isizez /)
+   p1 = (/ order, order, order /)
    p2 = (/ order, order, order /)
-   call Initialize(p1, p2, ads, ads_data, ierr)
+   call Initialize(n, p1, p2, p1-1, ads_test, ads_trial, ads_data, ierr)
 
    fullnorm = 0.d0
    iter = 0
    l2norm = 0.d0
    
-   call Step(iter, ComputePointForRHS, ads, ads_data, l2norm, ierr)
+   mix=1.d0
+   alpha_step=1.d0
+   nn=1.d0
+   ! call MultiStep(iter, mix, forcing, ads_test, ads_trial, ads_data,nn,alpha_step, l2norm, ierr)
+
+
+   call Step(iter, forcing, ads_trial, ads_data, l2norm, ierr)
    call MPI_Reduce(l2norm, fullnorm, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
    
-   if (MYRANK == 0) then
-      write(*, *)'L2 norm:', fullnorm
-      write(*,*) (abs(fullnorm - 1.d0) < epsilon)
-      if (prnt) then
-         write(*, *) 'Result:'
-         do i = 1, ads % s(3)
-            write(*, *) i, 'row='
-            do j = 1, ads % s(1) * ads % s(2)
-               write(*, *) ads_data % F(i,j)
-            enddo
-         enddo
-      endif
-      do i = 1, ads % s(1)
-         do j = 1, ads % s(2) * ads % s(3)
-            if (abs(ads_data % F(i,j) - 1.d0) > epsilon) then
-               ok = .FALSE.
-            endif
-         enddo
-      enddo
-      if (ok .eqv. .FALSE.) then
-         write(*,*) 'not OK'
-      endif
-   endif
+   ! if (MYRANK == 0) then
+   !    write(*, *)'L2 norm:', fullnorm
+   !    write(*,*) (abs(fullnorm - 1.d0) < epsilon)
+   !    if (prnt) then
+   !       write(*, *) 'Result:'
+   !       do i = 1, ads_tial % s(3)
+   !          write(*, *) i, 'row='
+   !          do j = 1, ads_tial % s(1) * ads_tial % s(2)
+   !             write(*, *) ads_data % F(i,j)
+   !          enddo
+   !       enddo
+   !    endif
+   !    do i = 1, ads % s(1)
+   !       do j = 1, ads_tial % s(2) * ads_tial % s(3)
+   !          if (abs(ads_data % F(i,j) - 1.d0) > epsilon) then
+   !             ok = .FALSE.
+   !          endif
+   !       enddo
+   !    enddo
+   !    if (ok .eqv. .FALSE.) then
+   !       write(*,*) 'not OK'
+   !    endif
+   ! endif
 
-   call Cleanup(ads, ads_data, ierr)
+   call Cleanup(ads_test, ads_trial, ads_data, ierr)
    call CleanParallelism(ierr)
 
 end program main
